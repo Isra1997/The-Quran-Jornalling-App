b907822e25ff0c921e405d94ead6e6b371902a91
Isra1997 <israragheb@gmail.com>
2022-11-21 14:52:40 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+
+import {  createTheme, Grid,  ThemeProvider } from '@mui/material';
+
 import './App.css';
+import AyaCard from './AyaCard/AyaCard';
+import Navbar from './Navbar/Navbar';
 
 function App() {
+  const theme = createTheme({
+    typography:{
+      fontFamily:['Roboto']
+    },
+    palette:{
+      primary:{
+        main:'#CEBFB3'
+      },
+      text:'#535058'
+    }
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+         <Navbar></Navbar>
+         <Grid container padding={10}>
+           <Grid xs={12}>
+              <AyaCard></AyaCard>
+           </Grid>
+           <Grid xs={12}>
+              <div>┈••✦☪︎✦••┈┈••✦☪︎✦••┈</div>
+           </Grid>
+         </Grid>
+    </ThemeProvider>
+  
   );
 }
 
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+
+
+
+export default function ElevateAppBar(props) {
+  return (
+    <>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6" component="div" marginLeft={10}>
+             The Quran Journaling App ✨
+
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        
+      <Toolbar />
+    </>
+  );
+}
